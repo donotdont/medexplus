4d919784ad1fc2ee66171e56a526014b7ca41b04
Chutirat Chaisan <donotdont1990@hotmail.com>
2023-12-29 15:09:07 +0700

    Update code nev

--- views/header.php
+++ views/header.php
@@ -59,7 +59,7 @@
     <div class="container d-flex flex-wrap">
       <ul class="nav me-auto">
         <li class="nav-item"><a href="#" class="nav-link link-dark px-2"><i class="fa-solid fa-phone"></i> 083 154 7593</a></li>
-        <li class="nav-item"><span  class="nav-link px-2">|</span></li>
+        <li class="nav-item"><span class="nav-link px-2">|</span></li>
         <li class="nav-item"><a href="#" class="nav-link link-dark px-2"><i class="fa-solid fa-envelope"></i> apdiagnostic11022022@gmail.com</a></li>
       </ul>
       <ul class="nav">
@@ -68,17 +68,24 @@
     </div>
   </nav>
 
-  <header class="py-3 mb-4 border-bottom sticky-top covermagin shadow-lg p-3 mb-5 bg-body">
-    <div class="container d-flex flex-wrap justify-content-center">
+  <nav class="py-3 mb-4 border-bottom sticky-top covermagin shadow-lg p-3 mb-5 bg-body navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container flex-wrap justify-content-center">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
       <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
         <img class="img-fluid" src="/assets/images/logo AP+MED.png" style="max-width: 180px;"></a>
       </a>
-      <ul class="nav nav-pills">
-        <li class="nav-item"><a class="nav-item nav-link" href="#About">About us</a></li>
-        <li class="nav-item"><a class="nav-item nav-link" href="#pp">Product</a></li>
-        <li class="nav-item"><a class="nav-item nav-link" href="#cont">Contact us</a></li>
-        <li class="nav-item"><a href="#" class="nav-link"><i class="fa-solid fa-user"></i></a></li>
-        <li class="nav-item"><a href="#" class="nav-link"><i class="fa-solid fa-cart-shopping"></i></a></li>
-      </ul>
+
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item"><a class="nav-item nav-link" href="#About">About us</a></li>
+          <li class="nav-item"><a class="nav-item nav-link" href="#pp">Product</a></li>
+          <li class="nav-item"><a class="nav-item nav-link" href="#cont">Contact us</a></li>
+          <li class="nav-item"><a href="#" class="nav-link"><i class="fa-solid fa-user"></i></a></li>
+          <li class="nav-item"><a href="#" class="nav-link"><i class="fa-solid fa-cart-shopping"></i></a></li>
+        </ul>
+      </div>
     </div>
-  </header>
+  </nav>
