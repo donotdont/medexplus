84c2e2802309d0de13a3464b8b23ebd80499d588
Chutirat Chaisan <donotdont1990@hotmail.com>
2024-01-03 21:12:53 +0700

    Update code

--- views/home.php
+++ views/home.php
@@ -1,20 +1,149 @@
 <div class="container">
-  <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="/assets/images/cover.png" class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="/assets/images/cover.png" class="d-block w-100" alt="...">
+
+  <!-- Top Deplay -->
+  <div class="row">
+    <div class="col-md-8">
+      <div id="carouselExampleCaptions" class="carousel slide">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="/assets/images/cover.png" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="/assets/images/cover.png" class="d-block w-100" alt="...">
+          </div>
+        </div>
       </div>
     </div>
+    <div class="col-md-4">
+      <div class="row" style="background-color:; height: 200px; margin-top: 15px;">
+      <img src="/assets/images/rect3.png" style="width: 430px; height: 200px;" >
+      <div class="col-4"></div>
+      <div class="col-4"> <a href="#" class="btn btn-success btn-sm" style="margin-top:-150px;">Buy Now</a></div>
+      <div class="col-4"></div>
+      </div>
+      <div class="row" style="background-color:; height:200px; margin-top: 50px;">
+      <img src="/assets/images/p2024.jpg" style="width: 430px; height: 200px;" >
+      </div>
+    </div>
+  </div>
+
+
+
+
+<!-- product Box -->
+<div class="row" style="margin-top: 200px;">
+
+<div class="col-md-12">
+  <h1>Recommended</h1>
+</div>
+
+<div class="row">
+<div class="col-md-3">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="/assets/images/pt-mi-11.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Coagulation Analyzer PT-M1-11</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-success">Buy Now</a>
   </div>
 </div>
 </div>
 
+<div class="col-md-3">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="/assets/images/p2024.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success">Buy Now</a>
+  </div>
+</div>
+</div>
+
+<div class="col-md-3">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="/assets/images/p2024.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success">Buy Now</a>
+  </div>
+</div>
+</div>
+
+<div class="col-md-3">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="/assets/images/p2024.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success">Buy Now</a>
+  </div>
+</div>
+</div>
+</div>
+</div>
 
 
 
 
 
 
+<div class="row" style="margin-top: 200px;">
+<div class="col-md-12">
+  <h1>Product</h1>
+</div>
+
+<div class="row">
+<div class="col-md-3">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="/assets/images/pt-mi-11.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Coagulation Analyzer PT-M1-11</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-success">Buy Now</a>
+  </div>
+</div>
+</div>
+
+<div class="col-md-3">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="/assets/images/p2024.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success">Buy Now</a>
+  </div>
+</div>
+</div>
+
+<div class="col-md-3">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="/assets/images/p2024.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success">Buy Now</a>
+  </div>
+</div>
+</div>
+
+<div class="col-md-3">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="/assets/images/p2024.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-success">Buy Now</a>
+  </div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+
+
+</div>
