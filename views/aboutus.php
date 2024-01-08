721e026af7cd768b96eb625638c2e012f76dfee9
Chutirat Chaisan <donotdont1990@hotmail.com>
2024-01-08 14:14:08 +0700

    Update code

--- views/aboutus.php
+++ views/aboutus.php
@@ -1,56 +1,155 @@
+<link rel="stylesheet" href="/assets/css/photoswipe.css">
+
 <!-- Our Team -->
-<div class="container-fluid box-aboustus" id="About">
+<div class="container-fluid ">
   <div class="container">
-    <div class="row" style="padding-top: 50px;">
-      <h4 class="style text-center"></h4>
+
+
+    <!-- about us -->
+    <div class="row head-about">
+      <h3>ABOUT US</h3><br>
+      <p>AP Diagnostic & MedExplus</p>
+    </div>
+    <div class="row" style="margin-top: 30px;">
+
+      <div class="col-md-8">
+        <p>
+          “บริษัท เอพี ไดแอกซ์นอสติก จำกัด” เป็นบริษัทนำเข้าและจัดจำหน่ายเครื่องตรวจวิเคราะห์ด้านเคมีคลีนิค ก่อตั้งขึ้นเมื่อปี พ.ศ. 2540 โดยคุณเด่น รวมสุข และคุณปุญชรัสมิ์ ปะระมะ ตั้งอยู่ในจังหวัดเชียงใหม่ ประเทศไทย
+
+          บริษัทฯ เริ่มต้นจากการนำเข้าเครื่องตรวจวิเคราะห์ด้านเคมีคลีนิคจากต่างประเทศ และให้บริการติดตั้งและบำรุงรักษาเครื่องให้กับโรงพยาบาลและสถานพยาบาลต่างๆ ในจังหวัดเชียงใหม่และจังหวัดใกล้เคียง ต่อมาบริษัทฯ ได้ขยายธุรกิจไปยังการให้บริการอบรมการใช้งานเครื่องตรวจวิเคราะห์ให้กับบุคลากรทางการแพทย์
+
+          ปัจจุบันบริษัทฯ นำเข้าและจัดจำหน่ายเครื่องตรวจวิเคราะห์ด้านเคมีคลีนิคจากผู้ผลิตชั้นนำระดับโลกหลายราย เช่น Dymind Biotechnology ,Biobase ,WE Med Lab และอื่นๆ บริษัทฯ มีทีมวิศวกรผู้เชี่ยวชาญที่พร้อมให้บริการติดตั้งและบำรุงรักษาเครื่องตรวจวิเคราะห์ให้กับลูกค้าทุกราย
+
+          “บริษัท เอพี ไดแอกซ์นอสติก จำกัด” มุ่งมั่นที่จะให้บริการที่มีคุณภาพแก่ลูกค้า โดยยึดมั่นในมาตรฐานความปลอดภัยและคุณภาพของผลิตภัณฑ์ พนักงานทุกคนของบริษัทฯ ได้รับการอบรมอย่างเข้มข้น เพื่อให้สามารถให้บริการแก่ลูกค้าได้อย่างมีประสิทธิภาพ
+
+          บริษัทฯ มีนโยบายที่จะพัฒนาธุรกิจอย่างต่อเนื่อง เพื่อตอบสนองความต้องการของลูกค้าและก้าวสู่การเป็นผู้นำด้านการให้บริการเครื่องตรวจวิเคราะห์ด้านเคมีคลีนิคในประเทศไทย</p>
+
+
+        <p>บริษัทฯ ได้รับการรับรองจากองค์กรต่างๆ ดังนี้</p>
+        <ul>
+          <li> ISO 9001:2015</li>
+          <li> ISO 13485:2016</li>
+          <li> CE Mark</li>
+        </ul>
+      </div>
     </div>
 
-    <div class="row">
-      <div class="col-md-6">
-        <div class="row justify-content-center">
-          <div class="card mb-3" style="max-width: 540px;">
-            <div class="row g-0">
-              <div class="col-md-4">
-                <div class="imgceo">
-                  <img src="/assets/images/ceo-den.jpg" class="card-img-top rounded-circle" alt="...">
+    <!-- about us -->
+
+
+
+
+
+    <!-- meet ceo -->
+    <div class="row head-about" style=" margin-top: 90px;">
+      <h3>CEO OF AP Diagnostic & MedExplus</h3><br>
+      <p></p>
+    </div>
+    <div class="row" style="margin-top: 30px;">
+      <!-- ceo Blog -->
+      <div class="row">
+        <div class="col-md-6">
+          <div class="row justify-content-center">
+            <div class="card mb-3" style="max-width: 540px;">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <div class="imgceo">
+                    <img src="/assets/images/ceo-den.jpg" class="card-img-top rounded-circle" alt="...">
+                  </div>
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h6 class="card-title">Den Roumsuk CEO </h6>
+                    <h6 class="card-title"> เด่น รวมสุข</h6>
+                    <p class="card-text">To be the leader of ecological chain integration in the field of life and health.</p>
+                    <p class="card-text"><small class="text-muted">Join us <i class="bi bi-facebook"></i> &nbsp;
+                        <i class="bi bi-instagram"></i> &nbsp; </small></p>
+                  </div>
                 </div>
               </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h6 class="card-title">Den Roumsuk CEO </h6>
-                  <h6 class="card-title"> เด่น รวมสุข</h6>
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-md-6">
+          <div class="row justify-content-center">
+            <div class="card mb-3" style="max-width: 540px;">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <div class="imgceo">
+                    <img src="/assets/images/ceo-women.jpg" class="card-img-top rounded-circle" alt="...">
+                  </div>
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h6 class="card-title">Puncharat Parlama CEO</h6>
+                    <h6 class="card-title">ปุญชรัสมิ์ ปะระมะ</h6>
+                    <p class="card-text">Explore life information and enhance life value.</p>
+                    <p class="card-text"><small class="text-muted">Join us <i class="bi bi-facebook"></i> &nbsp;
+                        <i class="bi bi-instagram"></i> &nbsp;</small></p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+    </div>
+    <!-- meet ceo -->
 
 
-      <div class="col-md-6">
-        <div class="row justify-content-center">
-          <div class="card mb-3" style="max-width: 540px;">
-            <div class="row g-0">
-              <div class="col-md-4">
-                <div class="imgceo">
-                  <img src="/assets/images/ceo-women.jpg" class="card-img-top rounded-circle" alt="...">
-                </div>
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h6 class="card-title">Puncharat Parlama CEO</h6>
-                  <h6 class="card-title">ปุญชรัสมิ์ ปะระมะ</h6>
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-              </div>
-            </div>
-          </div>
+    
+
+    <!-- meet our team -->
+    <div class="row head-about" style=" margin-top: 90px;">
+      <h3>EVENT</h3><br>
+      <h3>& OUR TERM</h3>
+    </div>
+
+    <div class="row" style=" margin-top: 30px;">
+      <div class="col col-12">
+        <div class="pswp-docs__home-gallery" id="gallery--header-home">
+          <figure class="pswp-docs__home-gallery-item">
+            <a href="/assets/images/eve234.jpg" data-pswp-srcset="" data-pswp-width="1300" data-pswp-height="1177" target="_blank" data-cropped="true">
+              <img srcset="/assets/images/eve234.jpg" src="" alt="">
+            </a>
+          </figure>
+          <figure class="pswp-docs__home-gallery-item">
+            <a style="padding-bottom:100%" href="/assets/images/cover1-1.jpg" data-pswp-width="1200" data-pswp-height="500" target="_blank" data-cropped="true">
+              <img sizes="(min-width: 1124px) 393px, 35vw" srcset="/assets/images/cover1-1.jpg" alt="">
+            </a>
+          </figure>
+          <figure class="pswp-docs__home-gallery-item">
+            <a style="padding-bottom:100%" href="/assets/images/support88.jpg" data-pswp-width="2500" data-pswp-height="1667" target="_blank" data-cropped="true">
+              <img sizes="(min-width: 1124px) 393px, 35vw" srcset="/assets/images/support88.jpg" src="" alt="">
+            </a>
+          </figure>
+          <figure class="pswp-docs__home-gallery-item">
+            <a style="padding-bottom:100%" href="/assets/images/ev23.jpg" data-pswp-width="2500" data-pswp-height="1667" target="_blank" data-cropped="true">
+              <img sizes="(min-width: 1124px) 393px, 35vw" srcset="/assets/images/ev23.jpg" alt="">
+            </a>
+          </figure>
+          <figure class="pswp-docs__home-gallery-item">
+            <a style="padding-bottom:100%" href="/assets/images/event12.jpg" data-pswp-width="720" data-pswp-height="960" target="_blank" data-cropped="true">
+              <img sizes="(min-width: 1124px) 393px, 35vw" srcset="/assets/images/event12.jpg" alt="">
+            </a>
+          </figure>
+          <div class="pswp-docs__home-gallery-credit">Photos by Luca Bravo</div>
         </div>
       </div>
     </div>
   </div>
 </div>
-<!-- Our Team -->
+<script type="module">
+  import PhotoSwipeLightbox from '/assets/js/photoswipe-lightbox.esm.js';
+  const lightbox = new PhotoSwipeLightbox({
+    // may select multiple "galleries"
+    gallery: '#gallery--header-home',
+
+    // Elements within gallery (slides)
+    children: 'a',
+    pswpModule: () => import('/assets/js/photoswipe.esm.js')
+  });
+  lightbox.init();
+</script>
